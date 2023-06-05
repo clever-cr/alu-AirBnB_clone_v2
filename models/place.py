f9ec3473c5ea2b65ab4e352a9552ccbeb0198751
kaybrian <kayonbrian@gmail.com>
2023-06-05 17:00:01 +0200

    places

--- models/place.py
+++ models/place.py
@@ -9,14 +9,16 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id", ondelete="CASCADE"),
+                     nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id", ondelete="CASCADE"),
+                     nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(string(1024), nullable=True)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True, default=0.0)
-    longitude = Column(Float, nullable=True, default=0.0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
